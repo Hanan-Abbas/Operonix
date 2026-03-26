f2e86b1d32506abff1f6eac08920f174f944ecc1
Hanan-Abbas <hananabbas0101@gmail.com>
2026-03-26 18:16:46 +0500

    Update event_bus.py

--- core/event_bus.py
+++ core/event_bus.py
@@ -14,3 +14,17 @@ class Event:
     def __str__(self):
         return f"[{self.timestamp}] {self.source} -> {self.name}: {self.data}"
 
+class EventBus:
+    def __init__(self):
+        self.listeners: Dict[str, List[Callable]] = {}
+        self.logger = logging.getLogger("EventBus")
+        self._queue = asyncio.Queue()
+
+    def subscribe(self, event_name: str, callback: Callable):
+        """Register a function to run when a specific event occurs."""
+        if event_name not in self.listeners:
+            self.listeners[event_name] = []
+        self.listeners[event_name].append(callback)
+        self.logger.info(f"Subscribed to {event_name}")
+
+    
