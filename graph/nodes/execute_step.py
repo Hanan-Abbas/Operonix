1244a7158be24aadf25971509ab56ee561f641a0
Hanan-Abbas <hananabbas0101@gmail.com>
2026-09-08 15:57:16 +0500

    Update execute_step.py

--- graph/nodes/execute_step.py
+++ graph/nodes/execute_step.py
@@ -14,6 +14,7 @@ from typing import Dict, Any
 
 from migration.graph_state import OperonixState
 from migration.domain_contracts import ExecutionRequest, ExecutionResult, TaskStatus
+from graph.trace_collector import get_trace_collector
 
 logger = logging.getLogger("Graph.ExecuteStep")
 
