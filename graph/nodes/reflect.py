a3b8e14ef888d108ef57cd338e451b61f706b0f0
Hanan-Abbas <hananabbas0101@gmail.com>
2026-09-23 17:04:23 +0500

    Update reflect.py

--- graph/nodes/reflect.py
+++ graph/nodes/reflect.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 
 import logging
 from typing import Dict, Any
-from datetime import UTC, datetime
+from datetime import timezone, datetime
 
 from migration.graph_state import OperonixState
 from graph.trace_collector import get_trace_collector
